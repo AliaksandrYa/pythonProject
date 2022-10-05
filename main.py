e4c5be0e2e7e789fe65e282de380700d0127d481
”AliaksandrYa” <alex728933@gmail.com>
2022-10-05 12:08:04 +0300

    TIK-001: add comments

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')  # передача в функцию
 def index():
     return render_template('index.html', name='My Name',
                            project_number=100, title='HOME PAGE')
+
